3cae8373e57b7b4f2cf1a160a783fa9366076ece
nligade78 <nikhilligade22@gmail.com>
2023-10-02 21:47:40 +0530

    Footer component coded

--- jobportel/src/Components/ValueDiv/Value.jsx
+++ jobportel/src/Components/ValueDiv/Value.jsx
@@ -58,6 +58,18 @@ export const Value = () => {
 
       </div>
 
+      <div className='card mt-[2rem] flex justify-between bg-blueColor p-[5rem] rounded-[10px]'>
+
+      <div>
+        <h1 className='text-blueColor text-[30px] font-bold'> Ready to switch Carrier</h1>
+        <h2 className='text-tectColor text-[25px] font-bold'>Let's get started</h2>
+     
+      </div>
+      <button className='border-[2px] rounded-[10px] py-[4px] px-[50px] text-[18px] font-semibold text-blueColor hover:bg-white border-blueColor'>
+          Get Started
+        </button>
+      </div>
+
     </div>
   )
 }
