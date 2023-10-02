3e31612351bfbea8a7f4c7e64f005a5491f573f9
nligade78 <nikhilligade22@gmail.com>
2023-10-02 11:12:27 +0530

    Search Component written

--- jobportel/src/App.jsx
+++ jobportel/src/App.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
 import NavBar from "./Components/NavBar/Navbar";
-import  Job  from "./Components/JobDiv/Job"
-import  Search  from "./Components/SearchDiv/Search"
-import  Footer  from "./Components/FooterDiv/Footer"
-import  Value  from "./Components/ValueDiv/Value"
+import Job from "./Components/JobDiv/Job"
+import Search from "./Components/SearchDiv/Search"
+import Footer from "./Components/FooterDiv/Footer"
+import Value from "./Components/ValueDiv/Value"
 
-function App  ()  {
+function App() {
   return (
-    <div>
-       <NavBar/>
-      <Job/>
-     
-      <Search/>
-      <Footer/>
-      <Value/>
-  </div>
+    <div className='w-[85%] m-auto bg-white'>
+      <NavBar />
+      <Search />
+      <Job />
+      <Footer />
+      <Value />
+    </div>
   )
 }
 
